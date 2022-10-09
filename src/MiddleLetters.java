75c19124130c4cb751f9e9096bb2dd213474364b
Sara García Martínez <saritagm8@gmail.com>
2022-10-09 16:44:11 +0200

    Completo version1

--- src/MiddleLetters.java
+++ src/MiddleLetters.java
@@ -5,28 +5,28 @@
 public class MiddleLetters {
 
     public static void main (String[]args){
-            String prueba ="margaritas";
 
-            char[] word = prueba.toCharArray();
-            int size = word.length;
-            String newWord;
-
-            if (size % 2 == 0) {
-                int firstLetter = size / 2;
-                int secondLetter = firstLetter ;
-                String letter1 = String.valueOf(word[firstLetter-1]);
-                String letter2 = String.valueOf(word[secondLetter]);
-                newWord = letter1 + letter2;
-
-            } else {
-                newWord = String.valueOf(word[size / 2 ]);
-            }
-
-
-            System.out.println(newWord);
+        System.out.println(middleLetter("margaritas"));
+        System.out.println(middleLetter("cat"));
+        System.out.println(middleLetter("mimic"));
 
 
 
     }
+    public static String middleLetter (String word){
+        char[] wordLetters = word.toCharArray();
+        int size = wordLetters.length;
+        String newWord;
 
+        if (size % 2 == 0) {
+            int firstLetter = (size / 2)-1;
+            String letter1 = String.valueOf(wordLetters[firstLetter]);
+            String letter2 = String.valueOf(wordLetters[firstLetter+1]);
+            newWord = letter1 + letter2;
+
+        } else {
+            newWord = String.valueOf(wordLetters[size / 2 ]);
+        }
+        return newWord;
+    }
 }
