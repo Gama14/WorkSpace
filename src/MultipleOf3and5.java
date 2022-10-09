b3d476a0f9044ddb21ca3d8ead30df57c8d5a287
Sara García Martínez <saritagm8@gmail.com>
2022-10-09 16:55:28 +0200

    Completo version1

--- src/MultipleOf3and5.java
+++ src/MultipleOf3and5.java
@@ -4,20 +4,25 @@ Dado un número entero encuentra los multiples de 3 i 5 de ese numero y los suma
  */
 public class MultipleOf3and5 {
     public static void main(String[] args) {
-        int number = 10;
 
+        System.out.println(sumOfMultiples(10));
+
+
+    }
+    public static int sumOfMultiples (int number){
+       //primero crea lista con numeros consecutivos del 1 al valor del parametro
         int[] numbers = new int[number];
-        for(int i =0; i<numbers.length;i++){
-            numbers[i]= i+1;
-
+        int size= numbers.length;
+        for(int i =0; i<size;i++){
+            numbers[i]= i;
         }
+
         int total = 0;
-        for (int j = 0; j < numbers.length-1; j++) {
+        for (int j = 0; j < size; j++) {
             if ((numbers[j] % 3 == 0) || (numbers[j] % 5 == 0)) {
                 total += numbers[j];
             }
         }
-
-        System.out.println(total);
+        return total;
     }
 }
